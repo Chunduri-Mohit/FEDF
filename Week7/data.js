fc9af2626dfdefd613b17d51fed31369c0616d00
Chunduri-Mohit <chundurimohit007@gmail.com>
2025-10-15 08:32:17 +0530

    Add files via upload

---
+++ Week7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+  { title: "Shutter Island", author: "Dennis Lehane", price: 999, availability: "in stock" },
+  { title: "BOB", author: "3vikram", price: 699, availability: "in stock" },
+  { title: "Percy Jackson", author: "Jenny Han", price: 99, availability: "out of stock" },
+  { title: "The Montek Project", author: "Ross Duffer", price: 500, availability: "in stock" }
+];
